7606bc571e484f2ce7635f1a8e00e18cd6e983f4
Giannis Tzonis <tzonis.john@hotmail.com>
2022-09-02 06:54:29 +0300

    forward

--- src/App.js
+++ src/App.js
@@ -5,8 +5,21 @@ import Box from "./Box";
 export default function App() {
   const [squares, setSquares] = React.useState(boxes);
 
+  function toggle(id) {
+    /**
+     * Challenge: use setSquares to update the
+     * correct square in the array.
+     *
+     * Make sure not to directly modify state!
+     *
+     * Hint: look back at the lesson on updating arrays
+     * in state if you need a reminder on how to do this
+     */
+  }
+
   const squareElements = squares.map((square) => (
-    <Box key={square.id} on={square.on} />
+    <Box key={square.id} id={square.id} on={square.on} toggle={toggle} />
   ));
+
   return <main>{squareElements}</main>;
 }
