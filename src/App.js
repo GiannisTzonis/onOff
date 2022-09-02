be430bd8a04c8da6a25c015e332bd04b6facd5c8
Giannis Tzonis <tzonis.john@hotmail.com>
2022-09-02 06:32:25 +0300

    functional

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from "react"
+import React from "react";
+import boxes from "./boxes";
+import Box from "./Box";
 
 export default function App() {
-    return (
-        <hi>Hi</hi>
-    )
+  const [squares, setSquares] = React.useState(boxes);
+
+  const squareElements = squares.map((square) => (
+    <Box key={square.id} on={square.on} />
+  ));
+  return <main>{squareElements}</main>;
 }
